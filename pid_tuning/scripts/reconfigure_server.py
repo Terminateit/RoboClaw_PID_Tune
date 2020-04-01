da4be938ac5a9a429dc3ece1ea211c9d6e409974
Ilia Sevostianov <ilya.terminator.27@gmail.com>
2020-04-01 23:40:40 +0300

    Files fixed

--- pid_tuning/scripts/reconfigure_server.py
+++ pid_tuning/scripts/reconfigure_server.py
@@ -3,10 +3,10 @@
 import rospy
 
 from dynamic_reconfigure.server import Server
-from reconfigure.cfg import ReconfigureConfig
+from pid_tuning.cfg import ReconfigureConfig
 
 def callback(config, level):
-    # rospy.loginfo("""Configure : {k_p}, {k_d}, {k_i}""".format(**config))
+    rospy.loginfo("""Configure : {k_p}, {k_d}, {k_i}""".format(**config))
     return config
 
 if __name__ == "__main__":

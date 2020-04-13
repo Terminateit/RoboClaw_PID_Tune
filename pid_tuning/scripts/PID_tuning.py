953640b3cf85297637f7e6e8792e1ceed4a8ee1c
Ilya Sevostyanovcd <ilya.terminator.27@gmail.com>
2020-04-13 22:22:47 +0300

    threading implemented

--- pid_tuning/scripts/PID_tuning.py
+++ pid_tuning/scripts/PID_tuning.py
@@ -4,7 +4,7 @@ import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
 from roboclaw import Roboclaw
-from threading import Thread, Lock
+import threading
 from math import sin, cos
 import time
 from scipy.spatial.transform import Rotation as Rot
@@ -13,52 +13,74 @@ from geometry_msgs.msg import PoseStamped, TwistStamped
 
 from dynamic_reconfigure.msg import Config # dynamics reconfigure
 
+from threading import Thread, Lock
+import sys 
 
 
 pi = 3.1415
-QPPS_rev = 2256
+tick_rev = 2256 #2421
 
-rc = Roboclaw("/dev/ttyACM0", 115200)
+port = sys.argv[1]
+rc = Roboclaw(port, 115200)
 rc.Open()
 address = 0x80
 
-# mutex = Lock()
-# L = rospy.get_param("base/width")
-# R = rospy.get_param("wheel_big/radius")
+mutex = Lock()
 
 config = Config() # dynamics reconfigure
 
-def pid_cb(data): # dynamics reconfigure callback
-    global config
-    config = data
+class PIDTuning_RoboClaw(Thread):
+    def __init__(self):
+        Thread.__init__(self)
 
-pid_sub = rospy.Subscriber('/pid_tuning/parameter_updates', Config, pid_cb) # dynamics reconfigure subscriber
+    def pid_cb(self, data): # dynamics reconfigure callback
+        global config
+        config = data
 
-rospy.init_node('pid_coefficients_tuning', anonymous=True)
+    def Manual(self):
+        rate = rospy.Rate(50)
 
-rate = rospy.Rate(100.0)
+        pub_sb = rospy.Subscriber('/pid_tuning/parameter_updates', Config, self.pid_cb)
 
-# wait for connection
-k_p = 0
-k_d = 0
-k_i = 0
-while not rospy.is_shutdown():
-    for i in range(len(config.doubles)):
-        if config.doubles[i].name == 'k_p':
-            k_p = config.doubles[i].value
-        if config.doubles[i].name == 'k_d':
-            k_d = config.doubles[i].value
-        if config.doubles[i].name == 'k_i':
-            k_i = config.doubles[i].value
+        while not rospy.is_shutdown():
+            if len(config.doubles) > 0:
 
-    print('k_p =',k_p, ', k_d =',k_d, ', k_i =',k_i)  
+                for i in range(len(config.doubles)):
+                    if config.doubles[i].name == 'k_p':
+                        self.k_p = config.doubles[i].value
+                    if config.doubles[i].name == 'k_d':
+                        self.k_d = config.doubles[i].value
+                    if config.doubles[i].name == 'k_i':
+                        self.k_i = config.doubles[i].value
+            else:
+                self.k_p = self.k_d = self.k_i = 5            
 
-    rc.SetM1VelocityPID(address, k_p, k_d, k_i, QPPS_rev)
-    rc.SetM2VelocityPID(address, k_p, k_i, k_d, QPPS_rev)
+            print('k_p=',self.k_p, ', k_d =',self.k_d, ', k_i =',self.k_i)  
+            mutex.acquire()
 
+            #----# Uncomment lines below in order to work with RoboClaw #----#
+
+            # rc.SetM1VelocityPID(address, self.k_p, self.k_d, self.k_i, tick_rev)
+            # rc.SetM2VelocityPID(address, self.k_p, self.k_i, self.k_d, tick_rev)
+
+            #----#
+            
+            # print('M1-PID-Velocity: ',rc.ReadM1VelocityPID(address), '\n')
+            # print('M2-PID-Velocity: ',rc.ReadM1VelocityPID(address), '\n')
+
+            mutex.release()
+            rate.sleep()   
+
+if __name__ == '__main__':
+    print("start PID Tuning")
+    PID_Tuning = PIDTuning_RoboClaw()
+
+    rospy.init_node('PID_Tuning', anonymous=True)
     
-    print('M1-PID-Velocity: ',rc.ReadM1VelocityPID(address), '\n')
-    print('M2-PID-Velocity: ',rc.ReadM1VelocityPID(address), '\n')
-    
-    rate.sleep()      
-
+    PID_Tuning.Manual()
+    while True:
+        try:
+            time.sleep(0.01)
+        except KeyboardInterrput:
+            PID_Tuning.join()
+            break
